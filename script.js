ccad3f2e1b69cf17cea2f6e91ac6862fbd51da27
shadab69 <shadab272000@gmail.com>
2024-07-30 13:04:36 +0530

    slider added

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 //         window.addEventListener("load", function(){
 //             loader.style.display = "none";
 //         })
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  // zoom: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay:{
+    delay: 5000,
+    loop: Infinity,
+  },
+});
